603d9eb85b50423910132beedab34d0af592d153
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-01-14 04:58:58 +0900

    Solution 정리

---
+++ src/main/java/Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution {
+
+    public int solution() {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution());
+
+    }
+
+}
